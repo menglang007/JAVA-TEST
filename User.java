d65e46519a6e975d25dae5512688bf088346b1e4
java-test <hkm6364@126.com>
2019-06-26 23:32:26 +0800

    add user.java

---
+++ User.java
@@ -0,0 +1,29 @@
+package com.offcn.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_user")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name="name",length = 200,nullable = true)
+    private String name;
+
+    @Column(name="age",length = 3,nullable = true)
+    private Integer age;
+
+}
